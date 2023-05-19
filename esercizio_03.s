871a99b71fb88b2c51987d3450d8aaa05b89ef55
Andrei Petrisor <126775153+petri2212@users.noreply.github.com>
2023-05-19 18:53:54 +0200

    pick an ascii value and send every bit on line 12

--- esercizio_03.s
+++ esercizio_03.s
@@ -1,42 +1,103 @@
-.data   0x10005000     
+# Un sistema basato sul microprocessore MIPS R2000 (clock pari a 500 MHz) è incaricato della trasmissione seriale
+# asincrona di una serie di caratteri ASCII memorizzati in un banco dell’area dati del sistema che funge 
+# da buffer di trasmissione. In particolare, il programma di cui si richiede la scrittura deve permettere 
+# di effettuare la trasmissione seriale asincrona mediante la linea 12 della cella di memoria INOUT. 
+# Detta linea deve essere mantenuta al livello logico alto quando non ci sono dati da trasmettere, mentre, 
+# quando un dato deve essere trasmesso, deve essere portata al livello logico basso per un tempo pari al tempo 
+# concesso per la trasmissione di un bit. Successivamente devono essere trasmessi in sequenza i singoli bit 
+# (dal più significativo al meno significativo), ciascuno con la durata riservata ai bit. Al termine della 
+# trasmissione del carattere ASCII, dovrà essere spedita la parità del messaggio (pari o dispari) e lo stop 
+# equivalente ad un livello logico alto per un tempo pari a 1 o 1.5 o 2 tempi di bit. 
+# Dopo aver trasmesso il bit di stop, potrà aver luogo la trasmissione del prossimo carattere. 
+# La linea 3 della cella di memoria a 16 bit INOUT fornisce l’informazione relativa alla velocità 
+# di trasferimento: 0 = 9600 bit/s (104 μs per bit) 1 = 19200 bit/s (52 μs per bit). 
+# La linea 2 della cella INOUT fornisce l’informazione relativa alla parità: 0 = parità pari; 
+# 1 = parità dispari. Le linee 14 e 15 della cella INOUT forniscono l’informazione relativa alla 
+# durata da associare al bit di stop: 00 = 1 tempo 01 = 1.5 tempi 10 o 11 = 2 tempi. 
+# Il programma da scrivere dovrà prelevare 32 caratteri ASCII memorizzati nella zona di memoria adibita 
+# a buffer di trasmissione e dovrà poi trasferirli serialmente. Alle celle di memoria sopra menzionate 
+# si assegnino indirizzi arbitrari che cadano, però, nell’area dei dati dell’architettura MIPS. 
+# Il programma deve essere assemblato, linkato e sottoposto a simulazione. Si faccia una stampa commentata 
+# del sorgente del programma realizzato (corredata anche del relativo flow-chart).
 
-INOUT:      .half 0xd10c    ##halfword, per settare il valore devo fare (set value of), indirizzo della word(in vui è contenuta la halfword), successivamente aggiungere l'intero valore della nuova word
 
+.data     
 
-ASCII:      .byte 0x41, 0x42, 0x43, 0x44
-            .byte 0x45, 0x46, 0x47, 0x48
-            .byte 0x49, 0x4A, 0x4B, 0x4C
-            .byte 0x4D, 0x4E, 0x4F, 0x50
-            .byte 0x51, 0x52, 0x53, 0x54
-            .byte 0x55, 0x56, 0x57, 0x58
-            .byte 0x59, 0x5A, 0x5B, 0x5C            
-            .byte 0x5D, 0x5E, 0x5F, 0x60
+INOUT:      .half 0xd10c                    # halfword, per settare il valore devo fare (set value of), 
+                                            # indirizzo della word(in vui è contenuta la halfword), successivamente aggiungere l'intero valore della nuova word
+ASCII:      .byte 0xc5, 0x42                      # 0x42, 0x43, 0x44.byte 0x45, 0x46, 0x47, 0x48.byte 0x49, 0x4A, 0x4B, 0x4C.byte 0x4D, 0x4E, 0x4F, 0x50.byte 0x51, 0x52, 0x53, 0x54.byte 0x55, 0x56, 0x57, 0x58.byte 0x59, 0x5A, 0x5B, 0x5C.byte 0x5D, 0x5E, 0x5F, 0x60
+
+STACK_RA:   .word 0x00000000
 
 .text
 
-MAIN:       addi $s0, 4097
-            la $t1 , INOUT          ## $t1 contiene l'indirizzo di INOUT
-
-START:      lh $s1, INOUT           ## Loads halfword
-            andi $s3, $s1, 0x1000        ## Checks linea 12, andi usa i valori in decimale e non in binario
-            beq $s3, $zero, START   ## IF linea 12 = 0 ripeti ciclo
-                                    ## 00000000 00000000   0111 0001 0000 0000   1100 
-                                    ## 
-RESET:      andi $s3, $s1, 0xEFFF   ## 00000000 00000000   1110 1111 1111 1111
-            sh $s3 , 0($t1)
 
 
-READ3:      andi $s3, $s1, 0x0008            ## Controllo bit linea 3              
-READ2:      andi $s3, $s1, 0x0004            ## Controllo bit linea 2  
-READ14_15:  andi $s3, $s1, 0xC000         ## Controllo bit linea 14 e 15
+READ_12:    lh $s1, INOUT                   # Loads halfword
+            andi $s2, $s1, 0x1000           # Checks linea 12, usiamo i valori in esadecimale
+            beq $s2, $zero, READ_12         # IF linea 12 != 0 ripeti ciclo                                    
+                                           
+READ_3:     andi $s3, $s1, 0x0008           # Controllo bit linea 3, se bit acceso ritorna valore 8 
+            jal CHECK_LOOP
+                            
+
+READ_ASCII: la $s7 , ASCII                  # read address of ascii
+            lb $t6, 0($s7)                  # prendo il carattere asci
+            addi $s7, 1                     # incremento di 1 la posizione dell'indirizzo ascii
+            addi $t5, 0x80
+            addi $t3, 9                     # inizializzo contatore a 9 perche lo decremento 8                  
+
+
+READ_BIT:   and $t2, $t6 , $t5              # se $t2 !=0 significa che bisogna fare set 
+            srl $t5, $t5, 1
+
+            beq $t2 , $zero , CHECK_BIT         # se $s3 != 0 salta a CHECK 
+            jal SET_LINE_12                   # salto linkato a loop_52                   
+            jal CHECK_LOOP 
+
+CHECK_BIT:  bne $t2 , $zero , GO     # se $s3 == 0 salta a LOOP_104 altrimenti viene saltato
+            jal RESET_LINE_12                    # salto linkato a loop_104
+            jal CHECK_LOOP
+
+GO:         addi $t3, -1
+            bne $t3 , $zero , READ_BIT
+
+
+MAIN:      addi $v1, 1
 
 
 
-            # Se la linea 12 è al livello logico basso devo capire
-            # come prelevare dalla linea 3 il bit che ci dirà 
-            # la velocità di trasmissione e quindi il tempo per il quale
-            # il livello logico basso deve essere mantenuto.
+SET_LINE_12:    la $t1 , INOUT                   # $t1 contiene l'indirizzo di INOUT
+                ori $s3, $s1, 0x1000             # 00000000 00000000   0001 0000 0000 0000 nomero 0xEFFF mette a 1 il bit 12
+                sh $s3 , 0($t1)                  # 00000000 00000000   0111 0001 0000 0000 
+                jr $ra                   
 
-            # Trasmissione dei bit del carattere ASCII
+RESET_LINE_12:  la $t1 , INOUT                   # $t1 contiene l'indirizzo di INOUT
+                andi $s3, $s1, 0xEFFF            # 00000000 00000000   1110 1111 1111 1111 nomero 0xEFFF mette a zero il bit 12
+                sh $s3 , 0($t1)                  # 00000000 00000000   0111 0001 0000 0000
+                jr $ra                  
 
+
+           
+READ_2:     andi $s4, $s1, 0x0004            # Controllo bit linea 2  
+READ_14_15: andi $s5, $s1, 0xC000            # Controllo bit linea 14 e 15
+
+                                             # LOOP_52 serve nel caso la trasmissione dei bit è settata a 1
+LOOP_52:    li $t0, 0x0008                   # 0x32C8
+LOOP_52_1:  addi $t0, -1    
+            bne $t0, $zero, LOOP_52_1
+            jr $ra      
+
+                                             # LOOP_104 serve nel caso la trasmissione dei bit è settata a 0
+LOOP_104:   li $t0, 0x000f                   # 0x6590 
+LOOP_104_1: addi $t0, -1    
+            bne $t0, $zero, LOOP_104_1
+            jr $ra  
             
+
+CHECK_LOOP: lw 
+            beq $s3 , $zero , CHECK_1         # se $s3 != 0 salta a CHECK 
+            jal LOOP_52                       # salto linkato a loop_52
+CHECK_1:    bne $s3 , $zero , END             # se $s3 == 0 salta a LOOP_104 altrimenti viene saltato
+            jal LOOP_104    
+END:        jr $ra            
